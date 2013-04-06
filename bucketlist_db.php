558d3bbc5dccf82b8231f5d0fbb4d48a23eb16dd
exiledesigns <exiledesigns@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-06 17:26:11 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/bucket-list/trunk@692766 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bucketlist_db.php
@@ -0,0 +1,439 @@
+<?php
+/*
+  *
+  * Exile plugin - Bucket List
+  * 
+  *
+  */
+ 
+class BucketListManager {
+
+ 	function addBucket( $data ) {
+	
+		#; Init DB
+		global $wpdb;
+		$bucketTableName = $wpdb->prefix . "bucketlist_bucket";
+		
+		#; Prepare the query
+		$order 			= $data['order'] ? $data['order'] : 0;
+		$title 			= $data['title'] ? $data['title'] : 'DefaultTitle';
+		$description 	= $data['description'] ? $data['description'] : 'DefaultDescription';
+		
+		#; Execute query
+		$wpdb->insert(
+			$bucketTableName, 
+			array( 
+				'title' 		=> $title,
+				'description'	=> $description,
+				'order' 		=> $order
+			)
+		);
+		
+		return $wpdb->insert_id;
+	}
+ 
+ 	function editBucket( $data ) {
+	
+		#; Init DB
+		global $wpdb;
+		$bucketTableName = $wpdb->prefix . "bucketlist_bucket";		
+		
+		#; Prepare the query
+		$title 			= $data['title'] ? $data['title'] : '';
+		$description 	= $data['description'] ? $data['description'] : '';
+		$bId 			= $data['id'] ? $data['id'] : "";
+		
+		if ( $bId != "" ) {
+		
+			#; Execute query
+			$wpdb->update( 
+				$bucketTableName, 
+				array( 
+					'title' 		=> $title,
+					'description'	=> $description
+				),
+				array( 'ID' => $bId ),
+				array( 
+					'%s',
+					'%s'
+				), 
+				array( '%d' ) 
+			);
+			
+			return true;
+		}
+		
+		return false;
+		
+	}
+	
+	function updateBucketOrder( $id, $nOrder ) {
+	
+		#; Init DB
+		global $wpdb;
+		$bucketTableName = $wpdb->prefix . "bucketlist_bucket";	
+		
+		if ( $id != "" ) {
+		
+			#; Execute query
+			$wpdb->update( 
+				$bucketTableName, 
+				array( 'order' => $nOrder ),
+				array( 'ID' => $id ), 
+				array( '%d'	), 
+				array( '%d' ) 
+			);
+			
+			return true;
+		}
+		
+		return false;
+		
+	}
+	 
+ 	function deleteBucket( $id ) {
+	
+		#; Init DB
+		global $wpdb;
+		$bucketTableName = $wpdb->prefix . "bucketlist_bucket";
+		$taskTableName = $wpdb->prefix . "bucketlist_task";
+		
+	 	#; Prepare the query
+		$qDelBucket = "DELETE FROM " . $bucketTableName . " WHERE id = %d";
+		
+		#; Execute query
+		$wpdb->query(
+			$wpdb->prepare(
+				$qDelBucket ,
+				$id
+			)
+		);
+		
+		#; Update task
+		$orderMax = $wpdb->get_var( 'SELECT COUNT(ID) FROM ' . $taskTableName . ' WHERE list_id = "1"') + 1;
+		$wpdb->update( 
+				$taskTableName, 
+				array( 'list_id' => 1, 'order' => $orderMax ),
+				array( 'list_id' => $id ), 
+				array( '%d', '%d' ), 
+				array( '%d' ) 
+			);
+		
+		return true;
+	}
+
+ 	function addTask( $data ) {
+	
+		#; Init DB
+		global $wpdb;
+		$taskTableName = $wpdb->prefix . "bucketlist_task";
+		
+		#; Prepare the query
+		$list_id 	= $data['list_id'] ? $data['list_id'] : '';
+		$order 		= $data['order'] ? $data['order'] : 0;
+		$title 		= $data['title'] ? $data['title'] : 'DefaultTitle';
+		
+		#; Execute query
+		$wpdb->insert(
+			$taskTableName, 
+			array( 
+				'title' 	=> $title,
+				'list_id'	=> $list_id,
+				'order' 	=> $order
+			)
+		);
+		
+		return $wpdb->insert_id;
+		
+	}
+
+ 	function editTask( $tId, $data ) {
+	
+		#; Init DB
+		global $wpdb;
+		$taskTableName = $wpdb->prefix . "bucketlist_task";
+		
+	 	#; Prepare the query		
+		$type = Array();
+		foreach( $data as $key => $value ){
+			if ( $key == 'title' || $key == 'description' || $key == 'date_checked' ) $type[] = '%s';
+			else $type[] = '%d';
+		}
+		
+		if ( $tId != "" ) {
+		
+			#; Execute query
+			$wpdb->update( 
+				$taskTableName, 
+				$data,
+				array( 'ID' => $tId ), 
+				$type, 
+				array( '%d' ) 
+			);
+			
+			if ( $data['date_checked'] == 'NULL' ) $wpdb->query( "UPDATE ". $taskTableName ." SET date_checked = NULL WHERE ID = ". $tId );
+			
+			return true;
+		}
+		
+		return false;
+	 
+	}
+	
+	function updateTaskOrder( $id, $nOrder, $bucket ) {
+	
+		#; Init DB
+		global $wpdb;
+		$taskTableName = $wpdb->prefix . "bucketlist_task";
+		
+		if ( $id != "" ) {
+		
+			#; Execute query
+			$wpdb->update( 
+				$taskTableName, 
+				array( 
+					'list_id'	=> $bucket,
+					'order' => $nOrder 
+				),
+				array( 'ID' => $id ), 
+				array( '%d', '%d' ), 
+				array( '%d' ) 
+			);
+			
+			return true;
+		}
+		
+		return false;
+		
+	}
+	
+ 	function deleteTask( $id ) {
+	
+		#; Init DB
+	 	global $wpdb;
+		$taskTableName = $wpdb->prefix . "bucketlist_task";
+		
+	 	#; Prepare the query
+		$qDelTask = "DELETE FROM " . $taskTableName . " WHERE id = %d";
+		
+		#; Execute query
+		$wpdb->query(
+			$wpdb->prepare(
+				$qDelTask ,
+				$id
+			)
+		);
+		
+		return true;
+	}
+	
+	function getListBucket( $withTask = true, $filtres = "" ){
+	
+		#; Init DB
+		global $wpdb;
+		$bucketTableName = $wpdb->prefix . "bucketlist_bucket";
+		$taskTableName = $wpdb->prefix . "bucketlist_task";
+		
+		#; Prepare the query
+		$qListBucket = "SELECT * FROM %s";
+	 	$where = "";
+		
+		#; Set the filter for the the query (if needed)
+		if ( $filtres ) {
+			if ( in_array( $filtres ) ) {
+				#; Handle the query filter for the task
+				if ( $filtres['title'] ) $where = " WHERE title LIKE '%" . $filtres['title'] . "%'";
+				if ( $filtres['id'] ) $where = $where == "" ? " WHERE id = " . $filtres['id'] : " AND id = " . $filtres['id'] ;
+				if ( $filtres['descrption'] ) $where = $where == "" ? " WHERE description LIKE '%" . $filtres['description'] ."%'" : " AND description LIKE '%" . $filtres['description'] . "%'" ;
+			}
+		}		
+		
+		#; Get results
+		$qListBucket .= $where.' ORDER BY ' . $bucketTableName . '.order'; 
+		$listBucket = $wpdb->get_results( $wpdb->prepare( $qListBucket, $bucketTableName ) );
+	 	
+		if ( $withTask ) {
+			#; Loop through bucket to get the task linked
+			$i = 0;
+			foreach ( $listBucket as $bucket ) {
+				$filtres['list_id'] = $bucket['id'];
+				$listTask = $this->getListTask( $filtres );
+				$listBucket[$i]['listTask'] = $listTask;
+				$i++;
+			}
+		}
+		 
+		#; Return the result tabs
+		return $listBucket;
+	}
+	
+	function getSelectListBucket( $filtres = "" ){
+	
+		#; Init DB
+		global $wpdb;
+		$bucketTableName = $wpdb->prefix . "bucketlist_bucket";
+		$taskTableName = $wpdb->prefix . "bucketlist_task";
+		
+		#; Prepare the query
+		$qListBucket = "SELECT * FROM " . $bucketTableName;
+	 	$where = "";
+		
+		#; Set the filter for the the query (if needed)
+		if ( $filtres ) {
+			if ( in_array( $filtres ) ) {
+				#; Handle the query filter for the task
+				if ( $filtres['title'] ) $where = " WHERE title LIKE '%" . $filtres['title'] . "%'";
+				if ( $filtres['id'] ) $where = $where == "" ? " WHERE id = " . $filtres['id'] : " AND id = " . $filtres['id'] ;
+				if ( $filtres['description'] ) $where = $where == "" ? " WHERE description LIKE '%" . $filtres['description'] ."%'" : " AND description LIKE '%" . $filtres['description'] . "%'" ;
+			}
+		}		
+		
+		#; Get results
+		$qListBucket .= $where.' ORDER BY ' . $bucketTableName . '.order'; 
+		$listBucket = $wpdb->get_results( $qListBucket );
+	 	$content = "";
+		foreach ( $listBucket as $bucket ) {
+			if ( $bucket->id == 1 ) $selected = ' selected="selected"';
+			else $selected = "";
+			$content .= '<option value="' . $bucket->id . '"'. $selected .'>' . $bucket->title . '</option>';
+		}
+		 
+		#; Return the result tabs
+		return $content;
+	}
+	
+	function getListBucketHTML( $withTask = true, $filtres = "" ){
+	
+		#; Init DB
+		global $wpdb;
+		$bucketTableName = $wpdb->prefix . "bucketlist_bucket";
+		$taskTableName = $wpdb->prefix . "bucketlist_task";
+		$content = '';
+		
+		#; Prepare the query
+		$qListBucket = "SELECT * FROM " . $bucketTableName;
+	 	$where = "";
+		
+		#; Set the filter for the the query (if needed)
+		if ( $filtres ) {
+			//if ( is_array( $filtres ) ) {
+				#; Handle the query filter for the task
+				if ( $filtres['title'] ) $where = " WHERE title LIKE '%" . $filtres['title'] . "%'";
+				if ( $filtres['id'] ) $where = $where == "" ? " WHERE id = " . $filtres['id'] : $where . " AND id = " . $filtres['id'] ;
+				if ( $filtres['description'] ) $where = $where == "" ? " WHERE description LIKE '%" . $filtres['description'] ."%'" : $where . " AND description LIKE '%" . $filtres['description'] . "%'" ;
+			//}
+		}		
+		
+		#; Get results
+		$qListBucket .= $where.' ORDER BY ' . $bucketTableName . '.order'; 
+		$listBucket = $wpdb->get_results( $qListBucket );
+		
+		#; Loop through bucket to prepare HTML
+		foreach ( $listBucket as $bucket ) {
+			
+			$content .= '<li id="bucket-' . $bucket->id . '">';
+			$content .= '<ul class="bucketlist-cat">';
+			$content .= '<li class="bucket_title"><span class="bucket_title"><label>' . stripslashes( $bucket->title ) . '</label>';
+			if ( $bucket->id != 1 ) $content .= '<a href="#" class="actions_deleteBucket" id="deleteBucketLink_' . $bucket->id . '"><img src="' . plugins_url() . '/bucketlist/images/delete.png" alt="delete" /></a>';
+			$content .= '<a href="#" class="actions_editBucket" id="editBucketLink_' . $bucket->id . '"><img src="' . plugins_url() . '/bucketlist/images/edit.png" alt="edit" /></a></span>';
+
+			if ( $withTask ) {
+				
+				$filtresTask['list_id'] = $bucket->id;				
+				$listTask = self::getListTask( $filtresTask );
+				
+				if ( $listTask ){
+					$content .= '<ul class="bucketcat-actions">';
+					$content .= '<li class="task_date_checked">' . __('Achieved on','cleio') . '</li>';
+					$content .= '<li class="task_link_to">' . __('Linked to post','cleio') . '</li>';
+					$content .= '<li class="link_hide_bucket">&nbsp;</li>';
+					$content .= '<li class="link_hide_bucket"><a href="#" id="hide-bucket_' . $bucket->id . '"><img src="' . plugins_url() . '/bucketlist/images/less.png" alt="less" /></a></li>';
+					$content .= '</ul>'; //.bucketlist-actions
+					$content .= '</li></ul>'; //.bucket-title + .bucketlist-cat
+				
+					$content .= '<ul class="connectedSortable bucketlist-items">';
+					foreach ( $listTask as $task ) {
+					
+						if ( $task->date_checked ) {
+							$checked=' checked="checked" ';
+							$date = new DateTime( $task->date_checked );
+							$dateFormat = $date->format('d-m-Y');	
+						}
+						else {
+							$checked = "";
+							$dateFormat = "&nbsp;";
+						}
+						
+						if ( $task->link_to ) {
+							$p = get_post( $task->link_to );
+							$linkFormat = '<input type="hidden" value="' . $task->link_to . '" id="task-link-value_' . $task->id . '" /><label style="display: none;">' . __("Not linked to any post yet.")  . '</label> <a href="#" class="edit-link-to" id="task-link_' . $task->id . '">' . $p->post_title .'</a>';
+						}
+						else {
+							$linkFormat =  '<input type="hidden" value="" id="task-link-value_' . $task->id . '" /><label>' . __("Not linked to any post yet.")  . '</label> <a href="#" class="edit-link-to" id="task-link_' . $task->id . '">' . __( "Do it!" ) .'</a>';						
+						}
+						
+						$content .= '<li class="bucketlist-item" id="task-' . $task->id . '">';
+							$content .= '<span><input type="checkbox" name="cb-task_'. $task->id .'"' . $checked . '/>&nbsp';
+							$content .= '<label>' . stripslashes( $task->title ) . '</label></span><ul class="bucketitem-actions">';
+							//$content .= '<li class="task_date_checked">25/12/2034' . $task->date_checked . '</li>';
+							$content .= '<li class="task_date_checked">' . $dateFormat . '</li>';
+							//$content .= '<li class="task_link_to"><a href="#">Long icelandic winter, a very long post [...]</a>' . $task->link_to . '</li>';
+							$content .= '<li class="task_link_to">' . $linkFormat . '</li>';
+							$content .= '<li class="task_delete"><a href="#" class="actions_deleteTask" id="deleteTaskLink_' . $task->id . '"><img src="' . plugins_url() . '/bucketlist/images/delete.png" alt="delete" /></a></li><li class="task_edit"><a href="#" class="actions_editTask" id="editTaskLink_' . $task->id . '"><img src="' . plugins_url() . '/bucketlist/images/edit.png" alt="edit" /></a></li>';
+						$content .= '</ul></li>';
+					}
+					$content .= '</ul>';
+				}
+				else {
+					$content .= '<ul class="bucketcat-actions">';
+					$content .= '<li class="task_date_checked"></li>';
+					$content .= '<li class="task_link_to"></li>';
+					$content .= '<li class="link_hide_bucket"></li>';
+					$content .= '<li class="link_hide_bucket"></li>';
+					$content .= '</ul>'; //.bucketlist-actions
+					$content .= '</li></ul>'; //.bucket-title + .bucketlist-cat
+					$content .= '<ul class="connectedSortable bucketlist-items"></ul>';
+				}
+			}
+			else {
+			
+			}
+			
+			$content .= '</li>';
+			
+		}
+		 
+		#; Return the result tabs
+		return $content;
+	}
+	
+	function getListTask( $filtres = "" ){
+		
+		#; Init DB
+		global $wpdb;
+		$$bucketTableName = $wpdb->prefix . "bucketlist_bucket";
+		$taskTableName = $wpdb->prefix . "bucketlist_task";
+		
+		#; Prepare the query
+		$qListTask = "SELECT * FROM " . $taskTableName;
+		$where = "";
+		
+		#; Set the filter for the the query (if needed)
+		if ( $filtres ) {
+			#; Handle the query filter for the task
+			if ( isset($filtres['title'] ) 	)	$where = " WHERE title LIKE '%" . $filtres['title'] . "%'";
+			if ( isset($filtres['list_id']) ) 	$where = $where == "" ? " WHERE list_id = " . $filtres['list_id'] : " AND list_id = " . $filtres['list_id'];
+			if ( isset($filtres['date'] ) 	)	$where = $where == "" ? " WHERE date > '" . $filtres['date'] . "'" : " AND date > '" . $filtres['date'] . "'";
+			if ( isset($filtres['id'] ) 	)	$where = $where == "" ? " WHERE id = " . $filtres['id'] : " AND id = " . $filtres['id'];
+			if ( isset($filtres['link_to']) ) 	$where = $where == "" ? " WHERE link_to = " . $filtres['link_to'] : " AND link_to = " . $filtres['link_to'];		
+		}		
+		
+		#; Get results
+		$qListTask .= $where . ' ORDER BY ' . $taskTableName . '.order'; 
+		return $wpdb->get_results( $qListTask );
+		
+	}
+	
+	
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,56 @@
+=== Plugin Name ===
+Contributors: exiledesigns
+Donate link: http://www.exiledesigns.com/
+Tags: bucket-list, bucket, list, to-do, achievements
+Requires at least: 3.0.1
+Tested up to: 3.4
+Stable tag: 4.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Set your goals, change your life, realize your dreams and share the inspiration of your achievements!
+
+== Description ==
+
+Bucket List lets you create, organize and beautifully show off all the goals you have in life.
+
+= Key Features =
+
+Through an elegant and straight-forward administration interface, you can:
+
+* Create as many goal categories and goals as you wish
+* Smoothly organize, reorder and edit categories/goals without reload (AJAX)
+* Mark a goal as achieved, and automatically register the date (you can as well modify the date)
+* Link an achieved goal to an existing Post/Page in your blog
+* Paste your Bucket List in any Post/Page using the [bucketlist] shortcode
+
+= Widget =
+
+We also included an optional widget that displays your latest achieved tasks. You can:
+
+* Customize widget title (i.e. My Latest Achievements)
+* Customize number of tasks displayed (5 by default)
+* Set up a link to your complete Bucket List page (or any other page)
+* Display or not the task category
+* Display or not the date of achievement
+ 
+
+== Screenshots ==
+
+1. Front-end
+2. Widget
+3. Back-end
+
+
+== Installation ==
+
+1. Browse to the Plugins Menu in Wordpress. Then either search for "Bucket List" through the Search Tab, OR upload the "bucketlist.zip" file through the Uploads tab.
+2. Activate the plugin by going back to the Plugins Menu.
+3. Setup and configure Bucket List by clicking the new "Bucket List" Wordpress Menu.
+4. To display the Bucket List in any of your Pages/Posts, simply paste this shortcode: **[bucketlist]**
+5. By default, the achievement date will show, but you can hide it: [bucketlist displaydate=0]
+
+
+== Credits ==
+
+Special thanks to [Brankic1979](http://www.brankic1979.com/) for his cute icons.
